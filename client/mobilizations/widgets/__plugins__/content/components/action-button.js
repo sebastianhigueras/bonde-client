67f92ce0c197202f28f6751a37165d654d495f35
Igor Santos <igr.exe@gmail.com>
2017-11-28 19:08:47 -0200

    feat(widgets/content): add cancel button like with message of confirm

--- client/mobilizations/widgets/__plugins__/content/components/action-button.js
+++ client/mobilizations/widgets/__plugins__/content/components/action-button.js
@@ -4,7 +4,7 @@ import classnames from 'classnames'
 const ActionButton = ({ children, editing, setState, onClick, title, style, className, value }) => (
   <button
     className={classnames('btn bg-blacker rounded', className)}
-    onClick={() => onClick(value)}
+    onClick={() => onClick(value, setState)}
     style={{
       position: 'relative',
       display: editing ? 'inline-block' : 'none',
