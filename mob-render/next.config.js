78400a04cb65130354e5d6b077cabc0affe37da1
Lucas Pirola <lucaspirola@gmail.com>
2017-12-13 18:11:18 -0200

    chore(webpack): Improve webpack js loader; Add packages required to add state to app;

--- mob-render/next.config.js
+++ mob-render/next.config.js
@@ -12,7 +12,23 @@ module.exports = {
         test: /\.css$/,
         // Simplest example (non-minified)..
         loader: `babel-loader!next-style-loader`
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /(node_modules|wysihtml-toolbar.min)/
+      },
+      {
+        test: /\.(png|jpg)$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            limit: 10000, // Convert images < 10k to base64 strings
+            name: 'assets/images/[name].[ext]'
+          }
+        }]
       }
+
     )
     return config
   }
